2099dd56a2784c7d6f8f761e025d59db8cb45674
Kang Jiaming <kangjm2005@hotmail.com>
2023-04-26 11:00:03 +0800

    fix: i18n

--- src/data/ja/list.js
+++ src/data/ja/list.js
@@ -1,47 +1,125 @@
 export const list = [
   {
     id: 1,
-    category: "科学探索",
-    title: "中国科学院大学実験探求 —— 計算光学イメージング",
-    duration: "3",
-    cost: "3000",
-    location: "中科院",
-    pdfUrl: "中国科学院大学実験探求--計算光学イメージング",
-    imgUrl: "光学成像_img.png",
+    category: "科学研究",
+    title: "中国科学院大学実験探究 —— 計算光学成像",
+    duration: "5",
+    cost: "6500",
+    accommodation:"4",
+    location: "北京",
+    pdfUrl: "1.中国科学院大学实验探究--计算光学成像.pdf",
+    imgUrl: "中国科学院大学实验探究 —— 计算光学成像.png",
   },
   {
     id: 2,
-    category: "科学探索",
-    title: "肺炎多糖結合ワクチンの調製と免疫価分析",
+    category: "科学研究",
+    title: "神経退行性疾患のマウスモデルの特性と病理学的解析",
     duration: "5",
     cost: "5000",
-    location: "",
+    location: "北京",
     pdfUrl:
-      "北大附中-ワクチン2-肺炎多糖結合ワクチンの調製と免疫価分析-過程工程所0625",
+        "2.神经退行性疾病小鼠模型的表征和病理分析.pdf",
+    imgUrl: "神经退行性疾病小鼠模型的表征和病理分析.png"
   },
   {
     id: 3,
-    category: "芸術メディア",
-    title: "障害児童支援 社会企業創業インキュベート 课儒",
-    duration: "3",
-    cost: "3000",
-    pdfUrl: "北大附中国际部研学企画.pptx(1)",
+    category: "科学研究",
+    title: "肺炎多糖結合ワクチンの調製と免疫価の分析",
+    duration: "5",
+    cost: "5000",
+    location: "北京",
+    pdfUrl:
+        "3.肺炎多糖结合疫苗的制备和免疫效价分析.pdf",
+    imgUrl: "肺炎多糖结合疫苗的制备和免疫效价分析.png"
   },
   {
     id: 4,
     category: "芸術メディア",
-    title: "民族文化ビジネスプラン 満天星",
-    duration: "3",
-    cost: "3000",
+    title: "北京青年報·【青年紀実】ディープニュース編集報道",
+    duration: "5",
+    cost: "5500-5700",
+    location: "北京",
     pdfUrl:
-      "【2022】創星計画——満天星民族文化創意開発とクロスボーダーコラボ研学キャンプ",
+        "北京青年报.pdf",
+    imgUrl: "北京青年报·【青年纪实】深度新闻采编报道.png"
   },
   {
     id: 5,
+    category: "芸術メディア",
+    title: "アート＆キュレーション・キャンプ・プロジェクト",
+    duration: "5",
+    cost: "5780",
+    location: "北京",
+    pdfUrl:
+        "5.艺术与策展营地项目.pdf",
+    imgUrl: "艺术与策展营地项目.png"
+  },
+  {
+    id: 6,
+    category: "芸術メディア",
+    title: "四川阿坝·映像プロパガンダテーマ",
+    duration: "7",
+    cost: "7600",
+    location: "四川",
+    accommodation: "6",
+    pdfUrl:
+        "6.川西阿坝·影像宣传主题.pdf",
+    imgUrl: "川西阿坝·影像宣传主题.png"
+  },
+  {
+    id: 7,
     category: "社会科学経済",
-    title: "金融 北青報 心理 青麟",
-    duration: "3",
-    cost: "3000",
-    pdfUrl: "金融商科集団実習",
+    title: "北京怡宁医院·臨床心理カウンセリングと精神疾患実習",
+    duration: "5",
+    cost: "5000-5200",
+    location: "北京",
+    pdfUrl:
+        "医院.pdf",
+    imgUrl: "北京怡宁医院·临床心理咨询和精神疾病实训.png"
+  },
+  {
+    id: 8,
+    category: "社会科学経済",
+    title: "アルツハイマー病の探究と認知症患者の臨床治療実習",
+    duration: "5",
+    cost: "5500-5700",
+    location: "北京",
+    pdfUrl:
+        "阿兹海默.pdf",
+    imgUrl: "阿尔茨海默症探究和失智老人临床疗愈实训.png"
+  },
+  {
+    id: 9,
+    category: "社会科学経済",
+    title: "黄山黟県·公益マーケティングテーマ",
+    duration: "7",
+    accommodation: "6",
+    cost: "7500",
+    location: "黄山",
+    pdfUrl:
+        "9.黄山黟县·公益营销主题.pdf",
+    imgUrl: "黄山黟县·公益营销主题.png"
+  },
+  {
+    id: 10,
+    category: "生態探検",
+    title:"甘南チベット族 - 遊牧文化と草原保護",
+    duration: "8",
+    accommodation: "7",
+    cost: "7900",
+    location: "甘肃",
+    pdfUrl: "10.甘南藏区 - 游牧文化与草原保育.pdf",
+    imgUrl: "甘南藏区 - 游牧文化与草原保育.png"
+  },
+  {
+    id: 11,
+    category: "生態探検",
+    title:"辛荘エコ・アートコミュニティ・団結と共同創作のコミュニティ",
+    duration: "5",
+    accommodation: "2",
+    cost: "5000-5500",
+    location: "北京",
+    pdfUrl: "11.辛庄生态·艺术社区·团结的社区，艺术的共建.pdf",
+    imgUrl: "辛庄生态·艺术社区·团结的社区，艺术的共建.png"
   },
 ];
