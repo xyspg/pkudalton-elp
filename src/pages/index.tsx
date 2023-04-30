62ecee635793d24a9f81e31c9275261069546b3f
xyspg <42668274+xyspg@users.noreply.github.com>
2023-04-30 17:21:14 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import Head from "next/head";
 import { useTranslations } from "next-intl";
 import Footer from "@/components/Footer";
+import Script from 'next/script'
 
 export default function Home() {
   const t = useTranslations("Head");
@@ -16,6 +17,7 @@ export default function Home() {
         <meta property="og:description" content={t("description")} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script src="https://analytics.umami.is/script.js" data-website-id="337d82f0-e60c-4977-ae98-c9c31aa49048" />
       <main className="h-screen">
         <Header />
         <div className="flex justify-center align-center">
